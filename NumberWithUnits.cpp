af53e2ee670474a0545d52c7883a3481ad4ca028
RotemHalbreich <rotem.halbreich@gmail.com>
2021-04-28 18:28:04 +0300

    doco addition

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -12,7 +12,7 @@ namespace ariel
     //Contructor that throws exception
     NumberWithUnits::NumberWithUnits(double quantity, const string &unit)
     {
-        if (!_myMap.contains(unit))
+        if (!_myMap.contains(unit)) // checks if the unit actually exists in the map
         {
             throw invalid_argument{"This unit type doesn't exists!"};
         }
@@ -27,24 +27,24 @@ namespace ariel
         double quantity_a = 0, quantity_b = 0;
         do
         {
-            file >> quantity_a >> unit_a >> eq >> quantity_b >> unit_b;
+            file >> quantity_a >> unit_a >> eq >> quantity_b >> unit_b; // reads from file, example: file >> 1km = 100000cm
 
-            _myMap[unit_b][unit_a] = 1.0 / quantity_b;
-            _myMap[unit_a][unit_b] = quantity_b;
+            _myMap[unit_b][unit_a] = 1.0 / quantity_b; // [unit_b]: <cm, <km, 1/100,000>>  note: map[][]-> inner map's value
+            _myMap[unit_a][unit_b] = quantity_b;       // [unit_a]: <km, <cm, 100,000>>    note: map[]-> inner map
 
             updateUnits(unit_a, unit_b);
             updateUnits(unit_b, unit_a);
 
-        } while (!file.eof());
+        } while (!file.eof()); //Run until the end of file
     }
 
     void NumberWithUnits::updateUnits(const string &unit_a, const string &unit_b)
-    {
-        for (auto &inner : _myMap[unit_a])
+    {                                      // ~~~EXAMPLE~~~
+        for (auto &inner : _myMap[unit_a]) // finds all maps related to [km]: [unit_a]: <km, <cm, 100,000>>
         {
-            double temp = _myMap[unit_b][unit_a] * inner.second;
-            _myMap[unit_b][inner.first] = temp;
-            _myMap[inner.first][unit_b] = 1.0 / temp;
+            double temp = _myMap[unit_b][unit_a] * inner.second; // [unit_b]: <cm, <km, 1/100,000 * 100,000>> -> temp = 1
+            _myMap[unit_b][inner.first] = temp;                  // <cm, <cm, 1>>
+            _myMap[inner.first][unit_b] = 1.0 / temp;            //
         }
     }
 
@@ -54,7 +54,7 @@ namespace ariel
         {
             return quantity;
         }
-        if (!_myMap[origin].contains(converted))
+        if (!_myMap[origin].contains(converted)) //if the unit you want to convert to doesn't exists in the map
         {
             throw invalid_argument{"Units do not match - [" + origin + "] cannot be converted to [" + converted + "]"};
         }
@@ -168,20 +168,20 @@ namespace ariel
         double quantity = 0;
         string unit;
 
-        is >> quantity >> ch;
+        is >> quantity >> ch; // starts reading the unit's quantity (1 for instance) and then what's next
         do
         {
-            if (ch != leftBracket)
-            {
-                unit.push_back(ch);
-            }
+            if (ch != leftBracket)  // keep reading chars one by one until you read ']'
+            {                       // if char isn't '[', read the unit's chars
+                unit.push_back(ch); // if char is '[' append it
+            }                       // example: you get -> [km]
             is >> ch;
         } while (ch != rightBracket);
-        if (!NumberWithUnits::_myMap.contains(unit))
-        {
+        if (!NumberWithUnits::_myMap.contains(unit)) // checks if the unit actually exists in the map
+        {                                            // example: if [km] actually exists
             throw invalid_argument{"This unit type doesn't exists!"};
         }
-        num._unit = unit;
+        num._unit = unit; // if the unit exists, it gets updated by type and quantity
         num._quantity = quantity;
         return is;
     }
